6acd920eb54a0085830eba9a94f85b1f104feb28
djtaylor <djtaylor@workvm.com>
2014-09-06 16:34:51 +0700

    Skeleton controller methods and template for all network sections.

--- python/cloudscape/portal/ui/app/network/controller.py
+++ python/cloudscape/portal/ui/app/network/controller.py
@@ -21,6 +21,18 @@ class AppController(PortalTemplate):
                 },
                 'routers': {
                     'data': self._routers
+                },
+                'switches': {
+                    'data': self._switches
+                },
+                'ipv4blocks': {
+                    'data': self._ipv4blocks
+                },
+                'ipv6blocks': {
+                    'data': self._ipv6blocks
+                },
+                'vlans': {
+                    'data': self._vlans
                 }
             },
             'default': 'overview'
@@ -28,7 +40,7 @@ class AppController(PortalTemplate):
         
     def _overview(self):
         """
-        Construct and return the template data required to render the host groups page.
+        Construct and return the template data required to render the network overview page.
         """
         
         # Make all required API calls
@@ -49,9 +61,201 @@ class AppController(PortalTemplate):
             }
         }
         
+    def _vlans(self):
+        """
+        Construct and return the template data required to render the network VLANs page.
+        """
+        
+        # Look for a target VLAN
+        vlan_target = self.get_query_key('vlan')
+        
+        # Make all required API calls
+        response = self.api_call_threaded({
+            'datacenters': ('locations', 'get_datacenters')
+        })
+        
+        def set_contents():
+            """
+            Set table contents.
+            """
+            if vlan_target:
+                return ['app/network/tables/vlans/details.html']
+            return ['app/network/tables/vlans/list.html']
+            
+        def set_popups():
+            """
+            Set popup contents.
+            """
+            if vlan_target:
+                return []
+            return [
+                'app/network/popups/vlans/create.html',
+                'app/network/popups/vlans/delete.html'
+            ]
+        
+        # Return the template data
+        return {
+            'vlans': {
+                'all':    None,
+                'detail': None,
+                'target': vlan_target
+            },
+            'datacenters': response['datacenters'],
+            'page': {
+                'header': None if not vlan_target else 'Network VLAN: %s' % vlan_target,
+                'title':  'CloudScape Network VLANs',
+                'contents': set_contents(),
+                'popups': set_popups()
+            }         
+        }
+        
+    def _ipv4blocks(self):
+        """
+        Construct and return the template data required to render the network IPv6 blocks page.
+        """
+        
+        # Look for a target block
+        block_target = self.get_query_key('block')
+        
+        # Make all required API calls
+        response = self.api_call_threaded({
+            'datacenters': ('locations', 'get_datacenters')
+        })
+        
+        def set_contents():
+            """
+            Set table contents.
+            """
+            if block_target:
+                return ['app/network/tables/ipv6blocks/details.html']
+            return ['app/network/tables/ipv6blocks/list.html']
+            
+        def set_popups():
+            """
+            Set popup contents.
+            """
+            if block_target:
+                return []
+            return [
+                'app/network/popups/ipv6blocks/create.html',
+                'app/network/popups/ipv6blocks/delete.html'
+            ]
+        
+        # Return the template data
+        return {
+            'blocks': {
+                'all':    None,
+                'detail': None,
+                'target': block_target
+            },
+            'datacenters': response['datacenters'],
+            'page': {
+                'header': None if not block_target else 'Network IPv6 Block: %s' % block_target,
+                'title':  'CloudScape Network IPv6 Blocks',
+                'contents': set_contents(),
+                'popups': set_popups()
+            }         
+        }
+        
+    def _ipv4blocks(self):
+        """
+        Construct and return the template data required to render the network IPv4 blocks page.
+        """
+        
+        # Look for a target block
+        block_target = self.get_query_key('block')
+        
+        # Make all required API calls
+        response = self.api_call_threaded({
+            'datacenters': ('locations', 'get_datacenters')
+        })
+        
+        def set_contents():
+            """
+            Set table contents.
+            """
+            if block_target:
+                return ['app/network/tables/ipv4blocks/details.html']
+            return ['app/network/tables/ipv4blocks/list.html']
+            
+        def set_popups():
+            """
+            Set popup contents.
+            """
+            if block_target:
+                return []
+            return [
+                'app/network/popups/ipv4blocks/create.html',
+                'app/network/popups/ipv4blocks/delete.html'
+            ]
+        
+        # Return the template data
+        return {
+            'blocks': {
+                'all':    None,
+                'detail': None,
+                'target': block_target
+            },
+            'datacenters': response['datacenters'],
+            'page': {
+                'header': None if not block_target else 'Network IPv4 Block: %s' % block_target,
+                'title':  'CloudScape Network IPv4 Blocks',
+                'contents': set_contents(),
+                'popups': set_popups()
+            }         
+        }
+        
+    def _switches(self):
+        """
+        Construct and return the template data required to render the network switches page.
+        """
+        
+        # Look for a target switch
+        switch_target = self.get_query_key('router')
+        
+        # Make all required API calls
+        response = self.api_call_threaded({
+            'datacenters': ('locations', 'get_datacenters')
+        })
+        
+        def set_contents():
+            """
+            Set table contents.
+            """
+            if switch_target:
+                return ['app/network/tables/switches/details.html']
+            return ['app/network/tables/switches/list.html']
+            
+        def set_popups():
+            """
+            Set popup contents.
+            """
+            if switch_target:
+                return []
+            return [
+                'app/network/popups/switches/create.html',
+                'app/network/popups/switches/delete.html'
+            ]
+        
+        # Return the template data
+        return {
+            'switches': {
+                'all':    None,
+                'detail': None,
+                'target': switch_target
+            },
+            'datacenters': response['datacenters'],
+            'page': {
+                'header': None if not router_target else 'Network Switch: %s' % switch_target,
+                'title':  'CloudScape Network Switches',
+                'contents': set_contents(),
+                'popups': set_popups()
+            }         
+        }
+        
     def _routers(self):
         """
-        Construct and return the template data required to render the hosts list page.
+        Construct and return the template data required to render the network routers page.
         """
         
         # Look for a target router

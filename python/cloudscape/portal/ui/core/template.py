ae05163fa57544d9bebdc06544f7d7469baf0c2c
djtaylor <djtaylor@workvm.com>
2014-09-06 16:25:58 +0700

    Incorrect path name in links, and fixed mis-declared class call.

--- python/cloudscape/portal/ui/core/template.py
+++ python/cloudscape/portal/ui/core/template.py
@@ -55,7 +55,7 @@ class PortalTemplate(object):
         """
         Return an HTTPResponseRedirect object.
         """
-        return HTTPResponseRedirect(location)
+        return HttpResponseRedirect(location)
      
     def get_query_key(self, key):
         """

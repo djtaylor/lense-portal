e3063c8745e71c2711221e82f3a17ba801299685
djtaylor <djtaylor@workvm.com>
2014-09-06 19:33:49 +0700

    Added some new module methods for the NetworkAPI client module.

--- python/cloudscape/client/module/network.py
+++ python/cloudscape/client/module/network.py
@@ -13,3 +13,33 @@ class NetworkAPI:
     
     def update_router(self, data=None):
         return self.parent._post(data=data, action='update', path='network/router')
+    
+    def add_router_interface(self, data=None):
+        return self.parent._post(data=data, action='add', path='network/router/interface')
+    
+    def remove_router_interface(self, data=None):
+        return self.parent._post(data=data, action='remove', path='network/router/interface')
+    
+    def get_ipv4_block(self, data=None):
+        return self.parent._get(data=data, action='get', path='network/block/ipv4')
+    
+    def create_ipv4_block(self, data=None):
+        return self.parent._post(data=data, action='create', path='network/block/ipv4')
+    
+    def delete_ipv4_block(self, data=None):
+        return self.parent._post(data=data, action='delete', path='network/block/ipv4')
+    
+    def update_ipv4_block(self, data=None):
+        return self.parent._post(data=data, action='update', path='network/block/ipv4')
+    
+    def get_ipv6_block(self, data=None):
+        return self.parent._get(data=data, action='get', path='network/block/ipv6')
+    
+    def create_ipv6_block(self, data=None):
+        return self.parent._post(data=data, action='create', path='network/block/ipv6')
+    
+    def delete_ipv6_block(self, data=None):
+        return self.parent._post(data=data, action='delete', path='network/block/ipv6')
+    
+    def update_ipv6_block(self, data=None):
+        return self.parent._post(data=data, action='update', path='network/block/ipv6')

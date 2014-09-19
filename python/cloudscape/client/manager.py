6f2acce001c47ac05c29beaeae6b1ef91b3880ca
djtaylor <djtaylor@workvm.com>
2014-09-19 12:29:53 +0700

    More verbose error printing and formatting in APIConnect

--- python/cloudscape/client/manager.py
+++ python/cloudscape/client/manager.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import requests
 
@@ -12,7 +13,7 @@ class APIConnect(object):
     Factory class used to construct an API connection object using the APIBase class. If supplying
     an API key, a token will be retrieved then passed off to the APIBase class.
     """
-    def __init__(self, user, group, api_key=None, api_token=None):
+    def __init__(self, user, group, api_key=None, api_token=None, cli=False):
         
         # API connection attributes
         self.api_user  = user       # API user
@@ -20,8 +21,11 @@ class APIConnect(object):
         self.api_key   = api_key    # API key
         self.api_token = api_token  # API token
         
-        # Token errors
-        self.token_err = {}
+        # Is this being run from the command line client
+        self.cli       = cli
+        
+        # Token response
+        self.token_rsp = None
         
         # Configuration
         self.conf      = config.parse()
@@ -51,16 +55,16 @@ class APIConnect(object):
             return True
         
         # Authentication URL
-        auth_url   = '%s/%s' % (self.api_url, PATH.GET_TOKEN)
+        auth_url       = '%s/%s' % (self.api_url, PATH.GET_TOKEN)
         
         # Get an API token
-        token_rsp  = parse_response(requests.get(auth_url, headers=self._get_token_headers()))
+        self.token_rsp = parse_response(requests.get(auth_url, headers=self._get_token_headers()))
     
         # Load the response body
-        auth_obj   = json.loads(token_rsp.get('body', {}))
+        auth_obj       = json.loads(self.token_rsp.get('body', {}))
     
         # If token request looks OK
-        if token_rsp['code'] == 200:
+        if self.token_rsp['code'] == 200:
             
             # Load the authorization token
             self.api_token = auth_obj['token']
@@ -71,14 +75,46 @@ class APIConnect(object):
         # Failed to retrieve a token
         else:
             
-            # Set the token error object
-            self.token_err = {
-                'code': token_rsp['code'],
-                'msg':  auth_obj.get('error', 'Failed to retrieve an authorization token')
-            }
-            
             # Return false
             return False
+    
+    def _error(self, message):
+        """
+        Handle any errors when creating an instance of the client. Different output depending
+        if the library is being called from the command line, or being loaded from another module.
+        """
+        
+        # If being run from the command line
+        if self.cli:
+            
+            # Show the error message
+            print 'ERROR: %s' % message
+            
+            # Show any problems with token retrieval
+            if self.token_rsp:
+                print 'HTTP %s: %s' % (self.token_rsp['code'], self.token_rsp['message'])
+                
+                # If any debug information is present
+                if 'debug' in self.token_rsp:
+                    print '---DEBUG---'
+                    print 'Traceback (most recent call last):'
+                    for l in self.token_rsp['debug']['traceback']:
+                        print '  File "%s", line %s, in %s' % (l[0], l[1], l[2])
+                        print '    %s' % l[3]
+                    print 'Exception: %s' % self.token_rsp['debug']['exception']
+            
+            # Exit the client
+            sys.exit(1)
+            
+        # Library is being loaded from another module
+        else:
+            
+            # If there was a problem with token retrieval
+            if self.token_rsp:
+                message += ' - HTTP %s: %s' % (self.token_rsp['code'], self.token_rsp['message'])
+            
+            # Raise an exception
+            raise Exception(message)
     
     def construct(self):
         """
@@ -87,11 +123,11 @@ class APIConnect(object):
         
         # Require an API key or token
         if not self.api_key and not self.api_token:
-            raise Exception('Must supply either an API key or token to make a request')
+            self._error('Must supply either an API key or a token to make a request')
         
         # Retrieve a token if not supplied
         if not self._get_token():
-            raise Exception('Failed to retrieve API token - HTTP %s: %s' % (self.token_err['code'], self.token_err['msg']))    
+            self._error('Failed to retrieve API token')  
             
         # API connector parameters
         self.params = {

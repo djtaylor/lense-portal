ebbb9633b1f8a1162cd46cd97942959340253e78
djtaylor <djtaylor@djtaylor-mbp.uni.workvm.com>
2015-07-21 11:34:51 -0400

    Tweaking the setup script.

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python
+import re
 import os
 import sys
 import apt
 import pip
 import imp
 import pwd
+import json
 import getpass
 import platform
-from getpass import getuser
+from getpass import getuser, getpass
+from subprocess import Popen, PIPE
 from cStringIO import StringIO
 
-# Installation home / feedback module
-HOME = os.path.expanduser('~')
-
-def _load_feedback():
-    """
-    Helper method to load the feedback module.
-    """
-    
+class _CAPTURE(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        sys.stdout = self._stdout
 
 class _BASE(object):
     """
@@ -28,15 +31,31 @@ class _BASE(object):
         Common attributes shared by inheriting classes.
         """
         
-        # Application user / home directory
+        # Application user / home directory / default login shell
         self.CS_USER    = 'cloudscape'
         self.CS_HOME    = '/home/cloudscape'
+        self.CS_SHELL   = '/bin/bash'
         
         # Get the absolute path to the source code directory
         self.SETUP_PATH = os.path.dirname(os.path.realpath(__file__))
     
         # Try to load the feedback module
         self.feedback   = self._load_feedback()
+    
+    def chown_dir_recursive(self, path, user):
+        """
+        Recursively change ownership of a directory.
+        """
+        
+        # Get user attributes
+        user_attr = pwd.getpwnam(self.CS_USER)
+        
+        # Change all files and directories
+        for root, dirs, files in os.walk(path):  
+            for dir in dirs:  
+                os.chown(os.path.join(root, dir), user_attr.pw_uid, user_attr.pw_gid)
+            for file in files:
+                os.chown(os.path.join(root, file), user_attr.pw_uid, user_attr.pw_gid)
     
     def _load_feedback(self):
         """
@@ -53,14 +72,18 @@ class _BASE(object):
         except Exception:
             self._die('Failed to load feedback module, path "%s" not found' % mod_path)
     
-    def _die(self, msg='An unknown error has occured', pre=None, post=None):
+    def _die(self, msg=None, pre=None, post=None):
         
         # Check for a pre-death method
         if callable(pre):
             pre()
         
         # Write the message on stderr
-        sys.stderr.write('ERROR: %s\n' % msg)
+        if msg:
+            if hasattr(self.feedback, 'show'):
+                self.feedback.show(msg).error()
+            else:
+                sys.stderr.write('ERROR: %s\n' % msg)
         
         # Check for a post-death method
         if callable(post):
@@ -130,7 +153,43 @@ class _PREFLIGHT(_BASE):
         """
         Helper method to create the cloudscape user account.
         """
-        self.feedback.show('Creating user').info()
+        
+        # Password prompt
+        passwd_prompt = lambda: (getpass('Please enter a password: '), getpass('Please confirm the password: '))
+        
+        # Get a password for the user
+        def _get_password():
+            passwd, passwd_confirm = passwd_prompt()
+            
+            # Make sure the passwords match
+            if not passwd == passwd_confirm:
+                self.feedback.show('Passwords do not match...').error()
+                return _get_password()
+            return passwd
+        
+        # Invoke the password prompt
+        passwd  = _get_password()
+        
+        # Add user command
+        adduser = ['/usr/sbin/useradd', '-m', '-d', self.CS_HOME, '-s', self.CS_SHELL, self.CS_USER]
+        
+        # Create the user account
+        user_proc = Popen(adduser, stderr=PIPE)
+        user_proc.communicate()
+        
+        # Make sure the account was created
+        if not user_proc.returncode == 0:
+            self._die('Failed to create user account: %s' % str(proc.stderr))
+        self.feedback.show('Created user account "%s"' % self.CS_USER).success()
+        
+        # Set the user's password
+        passwd_proc = Popen(['/usr/sbin/chpasswd'], stderr=PIPE, stdin=PIPE)
+        passwd_proc.communicate('%s:%s' % (self.CS_USER, passwd))
+        
+        # Make sure the password was set
+        if not passwd_proc.returncode == 0:
+            self._die('Failed to set account password: %s' % str(passwd_proc.stderr))
+        self.feedback.show('Set password for user account "%s"' % self.CS_USER).success()
         
     def _cs_user_exists(self):
         """
@@ -167,6 +226,9 @@ class _PREFLIGHT(_BASE):
                 if create_user == 'y':
                     self._cs_create_user()
         
+            # Prompt to create the user
+            _create_user()
+        
         # User exists
         else:
             self.feedback.show('User "%s" found, skipping creation...' % self.CS_USER).info()
@@ -196,30 +258,118 @@ class _PREFLIGHT(_BASE):
 
         # Make sure the cloudscape user account exists
         self._cs_user_exists()
-
-class _CAPTURE(list):
-    def __enter__(self):
-        self._stdout = sys.stdout
-        sys.stdout = self._stringio = StringIO()
-        return self
-    def __exit__(self, *args):
-        self.extend(self._stringio.getvalue().splitlines())
-        sys.stdout = self._stdout
+        
+        # Preflight complete
+        self.feedback.show('Completed setup preflight checks...').success()
 
 class _SETUP(_BASE):
     
     # APT/PIP/NPM packages
-    APT_PKGS = ['apache2', 'python-mysqldb', 'python-pip', 'python-ldap', 'node', 'nodejs', 'npm']
-    PIP_PKGS = ['socketIO-client', 'django', 'django-encrypted-fields', 'py3compat', 'django-auth-ldap']
+    APT_PKGS = ['apache2', 'python-mysqldb', 'python-dev', 'python-pip', 'python-ldap', 'node', 'nodejs', 'npm']
+    PIP_PKGS = ['socketIO-client', 'Django', 'django-encrypted-fields', 'py3compat', 'django-auth-ldap']
     NPM_PKGS = ['socket.io', 'ini', 'winston']
 
     def __init__(self):
-        super(_PREFLIGHT, self).__init__()
+        super(_SETUP, self).__init__()
 
+        # Flag to check if we are using a local MySQL server or not
+        self.mysql_use_local = False
+
+    def _set_apache_config(self):
+        """
+        Update the Apache configuration to read configuration files.
+        """
+        print '\n' \
+        'The setup script can automatically update your Apache\n' \
+        'configuration file to include Cloudscape virtual host\n' \
+        'configuration files. If you skip this step, you will have\n' \
+        'to update the main Apache configuration manually.\n'
+        
+        # Ask if we should update the Apache config file
+        def _update_config_prompt():
+            update_rsp = raw_input('Update the Apache configuration file? (y/n): ')
+            update_apache = update_rsp.lower()
+            if update_apache != 'y' or update_apache != 'n':
+                self.feedback.show('Answer must be "y" or "n"...').error()
+                return _update_config_prompt
+            return True if update_apache == 'y' else False
+
+        # If updating the config file
+        if update_apache:
+            return
+        else:
+            self.feedback.show('Skipping Apache configuration...').info()
+
+    def _get_npm_packages(self):
+        """
+        Get a list of installed NPM packages.
+        """
+        self.feedback.show('[npm]: Finding installed packages...').info()
+        npm_proc = Popen(['npm', '--json', '--prefix', self.CS_HOME, 'ls'], stdout=PIPE, stderr=PIPE)
+        npm_stdout, npm_stderr = npm_proc.communicate()
+
+        # If failed to retrieve packages
+        if not npm_proc.returncode == 0:
+            self._die('[npm]: Failed to discover installed packages...')
+        
+        # Return a JSON object of installed packages
+        json_pkgs = json.loads(npm_stdout)
+        if json_pkgs:
+            return json_pkgs['dependencies']
+        return json_pkgs
+
+    def _check_npm_packages(self):
+        """
+        Check for required NPM packages.
+        """
+        
+        # Node modules directory
+        node_modules_dir = '%s/node_modules' % self.CS_HOME
+        
+        # Make sure the node_modules directory exists
+        if not os.path.isdir(node_modules_dir):
+            os.mkdir(node_modules_dir, 0755)
+            self.feedback.show('[npm]: Created directory "%s"' % node_modules_dir).info()
+
+        # Construct the package list
+        npm_pkgs = self._get_npm_packages()
+        
+        # Packages to install
+        install_pkgs = []
+        
+        # Process each required package
+        self.feedback.show('[npm]: Checking for required packages...').info()
+        for pkg_name in self.NPM_PKGS:
+            if not pkg_name in npm_pkgs:
+                self.feedback.show('[npm]: Marking package "%s" for installation...' % pkg_name).info()
+                install_pkgs.append(pkg_name)
+            else:
+                self.feedback.show('[npm]: Package "%s" already installed...' % pkg_name).info()
+    
+        # Define the installation command
+        install_cmd = ['npm', '--prefix', self.CS_HOME, 'install'] + install_pkgs
+    
+        # Install any required packages
+        if install_pkgs:
+            self.feedback.show('[npm]: Installing missing packages...').info()
+            npm_proc = Popen(install_cmd, stderr=PIPE)
+            npm_proc.communicate()
+            
+            # Make sure packages were installed
+            if not npm_proc.returncode == 0:
+                self._die('[npm]: Failed to install missing packages: %s' % str(npm_proc.stderr))
+            self.feedback.show('[npm]: Package installation complete...').success()
+            
+            # Make sure ownership is correct
+            self.chown_dir_recursive(node_modules_dir, self.CS_USER)
+        else:
+            self.feedback.show('[npm]: All packages installed...').info()
+    
     def _get_pip_packages(self):
         """
         Get a list of installed PIP packages.
         """
+        self.feedback.show('[pip]: Finding installed packages...').info()
         with _CAPTURE() as output:
             pip.main(['list'])
             
@@ -236,41 +386,103 @@ class _SETUP(_BASE):
         Check for required PIP packages.
         """
         installed_pkgs = self._get_pip_packages()
-
-    def _install_apt_package(self, pkg_name, cache):
-        """
-        Install a package via apt-get
-        """
-        pkg = cache[pkg_name]
-        if pkg.is_installed:
-            print "[%s] Package already installed..." % pkg_name
-        else:
-            pkg.mark_install()
-
+        self.feedback.show('[pip]: Checking for required packages...').info()
+        
+        # Make sure required packages are installed
+        install_pkgs = []
+        for pkg_name in self.PIP_PKGS:
+            if not pkg_name in installed_pkgs:
+                self.feedback.show('[pip]: Marking package "%s" for installation...' % pkg_name).info()
+                install_pkgs.append(pkg_name)
+                    
+            # Package already installed
+            else:
+                self.feedback.show('[pip]: Package "%s" already installed...' % pkg_name).info()
+        
+        # If any packages need to be installed
+        if install_pkgs:
+            install_cmd = ['install'] + install_pkgs
+        
+            # Install any required packages
             try:
-                cache.commit()
-                print "[%s] Installing package..." % pkg_name
+                self.feedback.show('[pip]: Installing missing packages...').info()
+                pip.main(install_cmd)
+                self.feedback.show('[pip]: Package installation complete...').success()
+                
+            # Failed to install package
             except Exception as e:
-                print "[%s] Failed to install package: %s" % (pkg_name, str(e))
-                sys.exit(1)
+                self._die('[pip]: Failed to install missing packages: %s' % str(e))
+        else:
+            self.feedback.show('[pip]: All packages installed...').info()
+
+    def _check_mysql_server(self):
+        """
+        Give the option to install MySQL server on the local system.
+        """
+        def _mysql_local_prompt():
+            mysql_rsp = raw_input('Would you like to use a MySQL server on the local system? (y/n): ')
+            mysql_local = mysql_rsp.lower()
+            if mysql_local != 'y' and mysql_local != 'n':
+                self.feedback.show('Answer must be "y" or "n"...').error()
+                return _mysql_local_prompt()
+            return True if mysql_local == 'y' else False
+
+        # Check to see if we should use a local MySQL server
+        self.use_mysql_local = _mysql_local_prompt()
+        
+        # If using a local MySQL server
+        if self.use_mysql_local:
+            self.APT_PKGS.append('mysql-server')
+            self.feedback.show('Using local MySQL server...')
+        else:
+            self.feedback.show('Not using a local MySQL server...').info()
 
     def _check_apt_packages(self):
         """
-        Check for required APT packages.
+        Install a package via apt-get
         """
+        
+        # Apt cache object
+        self.feedback.show('[apt-get]: Building cache...').info()
         cache = apt.cache.Cache()
         cache.update()
-
-        # Look for required packages
-        print 'Checking for required apt packages...'
-        for _pkg in self.APT_PKGS:
-            self._install_apt_package(_pkg, cache)
+        
+        # Flag to run installer
+        run_install = False
+        
+        # Look at each required package
+        self.feedback.show('[apt-get]: Checking for required packages...').info()
+        for pkg_name in self.APT_PKGS:
+            pkg = cache[pkg_name]
+            if pkg.is_installed:
+                self.feedback.show('[apt-get]: Package "%s" already installed...' % pkg_name).info()
+            else:
+                run_install = True
+                self.feedback.show('[apt-get]: Marking package "%s " for installation' % pkg_name).info()
+                pkg.mark_install()
+        
+        # Install any required packages
+        if run_install:
+            try:
+                self.feedback.show('[apt-get]: Installing missing packages...').info()
+                cache.commit()
+                self.feedback.show('[apt-get]: Package installation complete...').success()
+                
+            # Failed to install package
+            except Exception as e:
+                self._die('[apt-get]: Failed to install missing packages: %s' % str(e))
+        else:
+            self.feedback.show('[apt-get]: All packages installed...').info()
 
     def run(self):
         """
         Run the initial setup utility.
         """
-        self._check_os_support()
+        self._check_mysql_server()
+        self._check_apt_packages()
+        self._check_pip_packages()
+        self._check_npm_packages()
+        self._set_apache_config()
 
 if __name__ == '__main__':
     
@@ -278,5 +490,6 @@ if __name__ == '__main__':
     preflight = _PREFLIGHT()
     preflight.run()
     
-    #setup = _SETUP()
-    #setup.run()
+    # Run the setup
+    setup = _SETUP()
+    setup.run()

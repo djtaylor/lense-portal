c7c4406e08888c5c2a45678c839738da5f2e8cf0
David Taylor <djtaylor13@gmail.com>
2015-10-08 10:58:55 -0400

    Added new 'lense' JS directory.

---
+++ usr/share/lense/static/portal/js/lense/app/home/interface.js
@@ -0,0 +1,14 @@
+cs.import('CSHomeInterface', function() {
+	
+	/**
+	 * Initialize CSHome
+	 * @constructor
+	 */
+	this.__init__ = function() {
+		
+		// Document ready
+		$(document).ready(function() {
+			
+		});
+	}
+});

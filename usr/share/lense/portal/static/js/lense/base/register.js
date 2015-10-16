2c9614ab6822284cdd144b33e45de3c7e3cb7207
David Taylor <djtaylor13@gmail.com>
2015-10-16 09:18:47 -0400

    Adding new file locations.

---
+++ usr/share/lense/portal/static/js/lense/base/register.js
@@ -0,0 +1,25 @@
+/**
+ * Lense Method Registration
+ */
+lense.import('LenseBaseRegister', function() {
+	
+	/**
+	 * Register Callback
+	 */
+	this.callback = function(n,m) {
+		if (!lense.hasOwnProperty('callback')) {
+			lense.callback = {};
+		}
+		lense.callback[n] = m;
+	}
+	
+	/**
+	 * Register Method
+	 */
+	this.method = function(n,m) {
+		if (!lense.hasOwnProperty('method')) {
+			lense.method = {};
+		}
+		lense.method[n] = m;
+	}
+});

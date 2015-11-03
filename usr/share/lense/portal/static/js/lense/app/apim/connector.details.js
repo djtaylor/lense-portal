dfb6f22bab654106189711bd1b3a2ab6b957f7c3
David Taylor <djtaylor13@gmail.com>
2015-11-03 09:41:29 -0500

    New templates and JS libraries

---
+++ usr/share/lense/portal/static/js/lense/app/apim/connector.details.js
@@ -0,0 +1,19 @@
+lense.import('LenseAPIMConnectorDetails', function() {
+	
+	/**
+	 * Intialize LenseAPIMConnectorDetails
+	 * @constructor
+	 */
+	this.__init__ = function() {
+		
+		// Document ready
+		$(document).ready(function() {
+			
+		});
+		
+		// Window resize
+		$(window).resize(function() {
+			
+		});
+	}
+});
--- usr/share/lense/portal/static/js/lense/exceptions.js
+++ usr/share/lense/portal/static/js/lense/exceptions.js
@@ -1,38 +1,38 @@
 /**
  * ModuleNotFound
  * 
- * Tried to load a module that was not imported using 'cs.import'.
+ * Tried to load a module that was not imported using 'lense.import'.
  * 
  * @param {m} The name of the module
  */
 function ModuleNotFound(m) {
 	this.name    = 'ModuleNotFound';
-	this.message = 'Module <' + m + '> not found, must import using <cs.import>';
+	this.message = 'Module <' + m + '> not found, must import using <lense.import>';
 }
 ModuleNotFound.prototype = Error.prototype;
 
 /**
  * MethodNotFound
  * 
- * Tried to run a method that was not registered in the 'cs.method' object.
+ * Tried to run a method that was not registered in the 'lense.method' object.
  * 
  * @param {m} The name of the method
  */
 function MethodNotFound(m) {
 	this.name    = 'MethodNotFound';
-	this.message = 'Method <' + m + '> not found, must register using <cs.register.method>';
+	this.message = 'Method <' + m + '> not found, must register using <lense.register.method>';
 }
 MethodNotFound.prototype = Error.prototype;
 
 /**
  * CallbackNotFound
  * 
- * Tried to run an API callback that was not registered in the 'cs.callback' object.
+ * Tried to run an API callback that was not registered in the 'lense.callback' object.
  * 
  * @param {c} The name of the callback
  */
 function CallbackNotFound(c) {
 	this.name    = 'CallbackNotFound';
-	this.message = 'Callback <' + c + '> not found, must register using <cs.register.callback>';
+	this.message = 'Callback <' + c + '> not found, must register using <lense.register.callback>';
 }
 CallbackNotFound.prototype = Error.prototype;

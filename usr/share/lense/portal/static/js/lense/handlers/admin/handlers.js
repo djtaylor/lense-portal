7f19d9eb1bd40cd2cf70158a58543d76606d8b92
David Taylor <djtaylor13@gmail.com>
2016-07-02 14:06:58 -0400

    Basic code for loading handlers in the portal

--- usr/share/lense/portal/static/js/lense/handlers/admin/handlers.js
+++ usr/share/lense/portal/static/js/lense/handlers/admin/handlers.js
@@ -1,3 +1,66 @@
 lense.import('admin.handlers', function() { 
+	var self = this;
 	
+	/**
+	 * Insert Handler Row
+	 */
+	this.insertHandlerRow = function(handler) {
+		$('#handlers-list').prepend(
+			'<tr handler="' + handler.uuid + '">' +
+			'<th scope="row"><input type="checkbox" handler="' + handler.uuid + '"></th>' +
+			'<th>' + handler.name + '</th>' +
+			'<th>' + handler.path + '</th>' +
+			'<th>' + handler.method + '</th>' +
+			'<th>' + handler['protected'] + '</th>' +
+			'<th>' + handler.enabled + '</th>' +
+			'</tr>'
+		);
+	}
+	
+	/**
+	 * Load Handlers Callback
+	 */
+	lense.register.callback('loadHandlers', function(data) {
+		if ($.isArray(data)) {
+			$.each(data, function(i,handler) {
+				self.insertHandlerRow(handler);
+			});
+		} else {
+			self.insertHandlerRow(data);
+		}
+	});
+	
+	/**
+	 * Constructor
+	 */
+	lense.register.constructor('admin.handlers', function() {
+		
+		// Load users
+		lense.api.request.submit('handler_get', null, 'loadHandlers');
+		
+		// Bind events
+		self.bind();
+	});
+	
+	/**
+	 * Create Handler
+	 */
+	this.createUser = function() {
+		lense.common.forms.validate('#create-handler-form');
+		
+		// Hide the modal and show the waiting dialog
+		//$('#create-handler').modal('hide');
+		//waitingDialog.show('Creating handler...');
+	}
+	
+	/**
+	 * Bind Events
+	 */
+	this.bind = function() {
+		
+		// Create user
+		$(document).on('click', '#create-handler-button', function() {
+			self.createHandler();
+		});
+	}
 });
